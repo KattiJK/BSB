fa5510d15cb6870babc2b135b5ad0bd7d3188321
Ekaterina <103816494+KattiJK@users.noreply.github.com>
2023-06-28 12:51:46 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+   $('.header__burger').click(function(event){
+      $('.header__burger,.header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+});
+
+// Таби
+const tabNavItems = document.querySelectorAll('.tabs-options__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	let currentActiveIndex = null;
+	let newActiveIndex = null;
+	if (targetElement.closest('.tabs-options__button')) {
+		tabNavItems.forEach((tabNavItem, index) => {
+			if (tabNavItem.classList.contains('active')) {
+				currentActiveIndex = index;
+				tabNavItem.classList.remove('active');
+			}
+			if (tabNavItem === targetElement) {
+				newActiveIndex = index;
+			}
+		});
+		targetElement.classList.add('active');
+		tabItems[currentActiveIndex].classList.remove('active');
+		tabItems[newActiveIndex].classList.add('active');
+	}
+});
